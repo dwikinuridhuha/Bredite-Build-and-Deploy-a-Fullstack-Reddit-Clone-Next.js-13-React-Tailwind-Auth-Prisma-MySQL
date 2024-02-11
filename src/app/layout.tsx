7d2afab85e21d73b8ff7836d005ab7e5d0597905
dwiki <dwiki.nuridhuha@gmail.com>
2024-02-12 05:15:03 +0700

    sampai di wqaaktu 17:00

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,20 @@
 import '@/styles/globals.css'
+import {Inter} from "next/dist/compiled/@next/font/dist/google";
+import {cn} from "../lib/utils";
 
 export const metadata = {
   title: 'Breadit',
   description: 'A Reddit clone built with Next.js and TypeScript.',
 }
 
+const inter = Inter({subsets:['latin']})
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' className={cn("bg-white text-slate-900 antialiased light", inter.className)}>
       <body>{children}</body>
     </html>
   )
