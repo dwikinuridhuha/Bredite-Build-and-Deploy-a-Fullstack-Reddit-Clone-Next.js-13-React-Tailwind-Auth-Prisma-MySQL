5cda72ec0a8f2e74404453b0f72fff4ed21dd8b2
dwiki <dwiki.nuridhuha@gmail.com>
2024-02-13 05:20:39 +0700

    sampai waktu ke 37:00

---
+++ src/components/SignIn.tsx
@@ -0,0 +1,11 @@
+import {FC} from 'react';
+
+const SignIn: FC = () => {
+    return (
+        <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
+            Sign In
+        </div>
+    )
+}
+
+export default SignIn;
